0ac6244180425e1f7b514dec976c563cdb2db3bb
Dan Fields <dhfp77@gmail.com>
2018-02-11 18:49:15 -0600

    Initial Tile class added.

---
+++ Tile.py
@@ -0,0 +1,19 @@
+class Tile:
+	def __init__(self, type, entity = None):
+		# Initialize with tile type, entity
+		# Set noise to 0 by default
+		# Tile type will be string, e.g. "FLOOR", "WALL", "DOOR"
+		self.type = type.upper()
+		self.entity = entity
+		self.noise = 0
+
+	def clear_noise(self):
+		self.noise = 0
+
+	# TO-DO: Add copy function
+
+# Testing example
+#example_tile = Tile("floor")
+#print(example_tile.type, example_tile.entity, example_tile.noise)
+
+
