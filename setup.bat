ce3a29dde26e8031a996a739eec0f3031891a18c
Chad Van Horn <vanhornchadm@gmail.com>
2018-03-18 14:46:54 -0500

    Intial setup.bat file

---
+++ setup.bat
@@ -0,0 +1,14 @@
+@echo off
+title Running Windows Setup Script
+reg query "hkcu\software\Python"
+if ERRORLEVEL 1 GOTO NOPYTHON
+goto :HASPYTHON
+:NOPYTHON
+echo This Computer does not have Python. Please install Python to Continue
+
+:HASPYTHON
+echo This Computer has Python. Proceeding with Setup
+pip install pyinstaller
+pyinstaller main.py
+
+pause
